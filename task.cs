9f073c5514f7b52d44352d7ac7fba221ae7a4f3c
suyoung <81684148+songsuyoung@users.noreply.github.com>
2023-01-05 20:56:42 +0900

    threadPool 먹통 문제 해결을 위한 Task 설정
    23.01.05

---
+++ task.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading; //thread를 이용하기 위해서 필요
+
+namespace ServerCore
+{
+	class Program
+	{
+		static void MainThread(object state) //object 타입을 이용해서 받아야한다는 점
+		{
+		Console.WriteLine("Hello world");
+		}
+	
+		static void Main(string[] args)
+		{
+			ThreadPool.SetMinThreads(1, 1);
+			//최소 쓰레드 설정
+			ThreadPool.SetMaxThreads(5, 5);
+			//최대 쓰레드 설정
+			
+			for(int i=0;i<5;i++)
+				Task t = new Task(() => { while (true) ; },TaskCreationOptions.LongRunning);
+			
+			ThreadPool.QueueUserWorkItem(MainThread); 
+			//위 task에서 선언된 TaskCreationOptions.LongRunning에 의해서 오랜 시간동안 작업할 테스크임을 미리
+			//빼놓는다. 그렇기 때문에 아래 ThreadPool에서 필요한 함수가 시작가능하다.
+			
+		}
+	
+	}
+}
